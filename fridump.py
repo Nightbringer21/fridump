0dbb19e5fc7c396ee636707113307929397000e2
0x90 <oleg.kupreev@gmail.com>
2018-02-22 05:22:05 +0300

    multiple bugs fixed

--- fridump.py
+++ fridump.py
@@ -13,39 +13,40 @@ import frida.core
 def dump_to_file(session, base, size, error, directory):
     """Reading bytes from session and saving it to a file """
     try:
-            filename = str(hex(base))+'_dump.data'
-            dump = session.read_bytes(base, size)
-            f = open(os.path.join(directory,filename), 'wb')
+        filename = os.path.join(directory, str(hex(base)) + '_dump.data')
+        dump = session.read_bytes(base, size)
+
+        with open(filename, 'wb') as f:
             f.write(dump)
-            f.close()
-            return error
-    except:
-           print("Oops, memory access violation!")
-           return error
+
+    except Exception as ex:
+        print("Oops, memory access violation! " + str(ex))
+
+    return error
 
 
 def splitter(session, base, size, max_size, error, directory):
     """Read bytes that are bigger than the max_size value, split them into chunks and save them to a file"""
-    times = size/max_size
+    times = int(size / max_size)
     diff = size % max_size
     if diff is 0:
-        logging.debug("Number of chunks:"+str(times+1))
+        logging.debug("Number of chunks:" + str(times + 1))
     else:
-        logging.debug("Number of chunks:"+str(times))
+        logging.debug("Number of chunks:" + str(times))
     global cur_base
     cur_base = base
 
     for time in range(times):
-            logging.debug("Save bytes: "+str(hex(cur_base))+" till "+str(hex(cur_base+max_size)))
-            dump_to_file(session, cur_base, max_size, error, directory)
-            cur_base = cur_base + max_size
+        logging.debug("Save bytes: " + str(hex(cur_base)) + " till " + str(hex(cur_base + max_size)))
+        dump_to_file(session, cur_base, max_size, error, directory)
+        cur_base = cur_base + max_size
 
     if diff is not 0:
-        logging.debug("Save bytes: "+str(hex(cur_base))+" till "+str(hex(cur_base+diff)))
+        logging.debug("Save bytes: " + str(hex(cur_base)) + " till " + str(hex(cur_base + diff)))
         dump_to_file(session, cur_base, diff, error, directory)
 
 
-def printProgress (times, total, prefix='', suffix='', decimals=2, bar=100):
+def printProgress(times, total, prefix='', suffix='', decimals=2, bar=100):
     """Progress bar function"""
     filled = int(round(bar * times / float(total)))
     percents = round(100.00 * (times / float(total)), decimals)
@@ -59,14 +60,14 @@ def printProgress (times, total, prefix='', suffix='', decimals=2, bar=100):
 def strings(filename, directory, min=4):
     """A very basic implementations of Strings"""
     strings_file = os.path.join(directory, "strings.txt")
-    path = os.path.join(directory,filename)
+    path = os.path.join(directory, filename)
 
-    str_list = re.findall("[A-Za-z0-9/\-:;.,_$%'!()[\]<> \#]+",open(path,"rb").read())
-    with open(strings_file,"ab") as st:
+    str_list = re.findall(b"[A-Za-z0-9/\-:;.,_$%'!()[\]<> \#]+", open(path, "rb").read())
+    with open(strings_file, "ab") as st:
         for string in str_list:
-            if len(string)>min:
+            if len(string) > min:
                 logging.debug(string)
-                st.write(string+b"\n")
+                st.write(string + b"\n")
 
 
 logo = """
@@ -85,50 +86,45 @@ def main():
     print(logo)
     parser = argparse.ArgumentParser(
         prog='fridump',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=textwrap.dedent(""))
 
     parser.add_argument('process', help='the process that you will be injecting to')
     parser.add_argument('-o', '--out', type=str, default=os.path.join(os.getcwd(), "dump"),
-                        help='provide full output directory path. (def: \'dump\')',
-                        metavar="dir")
+                        help='provide full output directory path.', metavar="dir")
     parser.add_argument('-u', '--usb', action='store_true', help='device connected over usb')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
     parser.add_argument('-r', '--read-only', action='store_true',
                         help="dump read-only parts of memory. More data, more errors")
     parser.add_argument('-s', '--strings', action='store_true',
                         help='run strings on all dump files. Saved in output dir.')
-    parser.add_argument('--max-size', type=int, help='maximum size of dump file in bytes (def: 20971520)',
+    parser.add_argument('--max-size', type=int, help='maximum size of dump file in bytes',
                         metavar="bytes", default=20971520)
+
     args = parser.parse_args()
 
-    # Define Configurations
-    PERMS = 'r--' if args.read_only else 'rw-'
-    LEVEL = logging.DEBUG if args.verbose else logging.INFO
-
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=LEVEL)
+    # Configure logging
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG if args.verbose else logging.INFO)
 
     # Start a new Session
     try:
         session = frida.get_usb_device().attach(args.process) if args.usb else frida.attach(args.process)
     except:
         print("Can't connect to App. Have you connected the device?")
-        sys.exit(0)
+        return 0
 
     # Selecting Output directory
-    print("Current Directory: " + str(os.getcwd()))
+    # print("Current Directory: " + str(os.getcwd()))
     print("Output directory is set to: " + args.out)
-    if os.path.isdir(args.out):
-        if not os.path.exists(args.out):
-            print("Creating directory...")
-            os.makedirs(args.out)
+    if not os.path.exists(args.out):
+        print("Creating directory...")
+        os.makedirs(args.out)
 
     mem_access_viol = ""
-
-    print("Starting Memory dump...")
-    Memories = session.enumerate_ranges(PERMS)
-
+    Memories = session.enumerate_ranges('r--' if args.read_only else 'rw-')
     i, l = 0, len(Memories)
+    print("Starting Memory dump for %i memory ranges..." % l)
+
     # Performing the memory dump
     for memory in Memories:
         base = memory.base_address
@@ -147,7 +143,7 @@ def main():
     print()
 
     # Run Strings if selected
-    if args.string:
+    if args.strings:
         files = os.listdir(args.out)
         i = 0
         l = len(files)
@@ -158,7 +154,7 @@ def main():
             printProgress(i, l, prefix='Progress:', suffix='Complete', bar=50)
 
     print("Finished!")
-    return 0
+    return 1
 
 
 if __name__ == '__main__':
