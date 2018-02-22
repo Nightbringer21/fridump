15ea28c94feeae5a3dc0df72c8aaece501a23fb8
0x90 <oleg.kupreev@gmail.com>
2018-02-22 04:55:26 +0300

    .gitignore added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fridump',
+    version='0.1.2',
+    packages=[''],
+    url='https://github.com/Nightbringer21/fridump',
+    license='GPL',
+    author='Nightbringer21',
+    author_email='test@email.me',
+    description='Fridump is using the Frida framework to dump accessible memory addresses from any platform supported.'
+)
